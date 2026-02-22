fd4becc6ec8153714604d08e2b1a58daa8785b4c
VivianQui <viloqui@hotmail.com>
2026-02-22 16:08:10 -0500

    Configuración en urls

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from miapp.views import lista_posts, detalle_post
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lista_posts, name='lista_posts'),
     path('post/<int:post_id>/', detalle_post, name='detalle_post'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
