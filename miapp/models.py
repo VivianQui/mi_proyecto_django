2a8fbc72c4b72d92bb15645498fad8aefebe15fd
VivianQui <viloqui@hotmail.com>
2026-02-26 13:24:43 -0500

    Agrego campo imagen al modelo Post

--- miapp/models.py
+++ miapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     contenido = models.TextField()
+    imagen = models.ImageField(upload_to='posts/', null=True, blank=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
